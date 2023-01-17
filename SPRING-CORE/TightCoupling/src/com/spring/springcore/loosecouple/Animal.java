0cdfafb04942e3a06d87b08138c0a8285c78984a
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2023-01-17 10:24:59 +0530

    Delete SPRING-CORE directory

--- SPRING-CORE/TightCoupling/src/com/spring/springcore/loosecouple/Animal.java
+++
@@ -1,5 +0,0 @@
-package com.spring.springcore.loosecouple;
-
-public interface Animal {
-    void handleAnimal();
-}
--- SPRING-CORE/TightCoupling/src/com/spring/springcore/loosecouple/Cat.java
+++
@@ -1,8 +0,0 @@
-package com.spring.springcore.loosecouple;
-
-public class Cat implements Animal {
-    @Override
-    public void handleAnimal() {
-        System.out.println("animal handled");
-    }
-}
